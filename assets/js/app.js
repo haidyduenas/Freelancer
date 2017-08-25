584874f630ece941aef34d1a1c317e178cc57550
haidy <haidy>
2017-08-25 06:56:40 -0500

    Maquetacion box

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,11 @@
 /*
  * Archivo principal de JS
  */
- 
+ $(".col-4").click(function(p){
+	var img = p.target.src;
+	var modal = '<div class="modal" id ="modal"><img src="'+ img +'" class="modal_img"><div class="modal_boton" id="modal_boton">X</div></div>'
+	$("body").append(modal);
+	$("#modal_boton").click(function(){
+		$("#modal").remove();
+	})
+})
